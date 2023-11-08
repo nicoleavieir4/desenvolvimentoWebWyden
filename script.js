2a9e1cd9f5ac9f46f0a1f0a1d9dda45081f8a03b
nicoleavieir4 <nicoleavieira@icloud.com>
2023-11-07 23:40:39 -0300

    iniciando projeto final desenvolvimento web

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-// Altera o texto do título
-document.querySelector("h1").textContent =
-  "Programação e Sistemas de Informação";
+function openPacman() {
+  const gameContainer = document.getElementById("game-container");
+  gameContainer.innerHTML = '<div class="pacman-css"><div class="base"></div></div>';
+}
 
-// Adiciona um link para o GitHub
-document.querySelector(
-  "footer"
-).innerHTML += `<a href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a>`;
+function openTicTacToe() {
+  // Implemente a lógica para abrir o Jogo da Velha
+}
+
+function openPingPong() {
+  const gameContainer = document.getElementById("game-container");
+  
+  // Remova qualquer conteúdo anterior dentro do game-container
+  gameContainer.innerHTML = '';
+  
+  // Crie um novo elemento para a animação dentro do game-container
+  const pingPongAnimation = document.createElement("div");
+  pingPongAnimation.classList.add("ping-pong-simulation");
+
+  // Adicione o elemento da animação ao game-container
+  gameContainer.appendChild(pingPongAnimation);
+}
+
+function openCasino() {
+  // Implemente a lógica para abrir o Cassino
+}
+
+function openCardGames() {
+  // Implemente a lógica para abrir Jogos de Cartas
+}
